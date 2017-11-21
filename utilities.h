3445f22366485baff2850459282a78c8a8c74fd1
Alina <alipercu@gmail.com>
2017-11-20 22:45:17 -0500

    includes and globals

--- utilities.h
+++ utilities.h
@@ -2,4 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+#include <sys/ipc.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+struct Message {
+	int to;
+	int from;
+	int type;
+	int request;
+	char content[1024];
+};
