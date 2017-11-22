443ef91eed590186016869f9ceaa0b9e2f97edb7
Alina <alipercu@gmail.com>
2017-11-21 23:03:30 -0500

    server global queue

--- server.c
+++ server.c
@@ -4,7 +4,10 @@ int main(int argc, char **argv){
 
 	//server globals
 	struct Message msg_received;
+	int printer_queue, request_queue, reply_queue;
+	int status;
 
 	// create the queues
+
 	return 0;
 }
