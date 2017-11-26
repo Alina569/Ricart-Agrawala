3e5e3fb7b359b950a3fff0320ce6d6eb91a90132
Alina <alipercu@gmail.com>
2017-11-26 00:02:07 -0500

    Basic working hacker

--- hacker.c
+++ hacker.c
@@ -1,4 +1,32 @@
+#include "utilities.h"
 
 int main(int argc, char **argv){
+	srand(time(NULL));
+
+	struct Message message;
+	int status;
+	int printer_queue = new_queue(PRINTER);
+
+	char buffer[1024] = {"Hacker ja ja ja"};
+
+	while(TRUE){
+		int sleep_time = get_random(2,4);
+
+		message.to = PRINTER;
+		message.from = getpid();
+		message.type = 0;
+		message.request = 0;
+
+		sleep(sleep_time);
+
+		strcpy(message.content, buffer);
+		status = msgsnd(printer_queue, &message, 1024, 0);
+
+		if (status == -1){
+			printf("Failed message");
+			exit(-1);
+		}
+		printf("Message sent");
+	}
 	return 0;
 }
