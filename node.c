0ae2b3275f27a92544e0ca420e43ca0fcda47ef2
Alina <alipercu@gmail.com>
2017-11-29 23:26:37 -0500

    node guideline

--- node.c
+++ node.c
@@ -1,4 +1,14 @@
+#include "utilities.h"
 
 int main(int argc, char **argv){
+	pid_t pid = getpid(); // proc id
+
+	// enter the node number
+	// get sh
+	// create queues
+	// create sems
+	// send messages connecting ....
+	// attach sm
+
 	return 0;
 }
