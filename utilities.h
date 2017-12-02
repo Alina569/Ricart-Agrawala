6821c4930d5d8ca4289e4774089c3a1d60d6b8cf
Alina <alipercu@gmail.com>
2017-12-02 13:56:16 -0500

    global functions

--- utilities.h
+++ utilities.h
@@ -44,7 +44,7 @@ int new_queue(int pKey){
 };
 
 char* concant(const char *string1, const char *string2){
-	const size_t len_str1 = strlen(string1);
+
 	const size_t len_str2 = strlen(string2);
 
 	char *result = malloc(len_str1 + len_str2 + 1);
@@ -53,3 +53,28 @@ char* concant(const char *string1, const char *string2){
 
 	return result;
 };
+
+// node and messages functions
+
+int get_node(int id, int *shared_memory){
+	int i;
+	for (i = 1; i < shared_memory[1]; i++){
+		if (shared_memory[200 + i] == id) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int send_message(int to, int queue, int type, char buffer[1024], int *shared_memory){
+	struct Message message;
+
+	message.to = to;
+	message.from = shared_memory[0];
+	message.type = type;
+	message.request = shared_memory[2];
+	strcpy(message.content, buffer);
+
+	return msgsnd(queue, &message, MSG_QUEUE_SIZE, 0);
+
+}
